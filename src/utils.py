b6bc3011d385f28192863778da1a3e09f6fcd972
zztaouil <ztaouil@outlook.com>
2026-01-05 16:07:43 +0100

    [feat] arithmetic operations

--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,29 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-from random import randint
-
-def display_img(img):
+def display_img(img, title=None):
     # converting color space BGR (default for cv2.imread) to RGB (what plt expects)
     # example color spaces: Grayscale, HSV (Hue, Saturation, Value),
     #   RGBA (Red, Green, Blue, Alpha)
     # https://opencv.org/blog/color-spaces-in-opencv/
+    plt.title(title)
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     # plt.imshow(img[:,:,::-1])
     plt.axis('off')
     plt.show()
- 
+
+def display_mul_img(imgs, titles=None):
+    if titles and len(imgs) != len(titles):
+        return 1
+    for i, img in enumerate(imgs):
+        plt.subplot(1, len(imgs), 1 + i)
+        if titles:
+            plt.title(titles[i])
+        plt.axis('off')
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
+    return 0   
+
 def draw_noise(img):
     pixels_nb = img.size // 5
     

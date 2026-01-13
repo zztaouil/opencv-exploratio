d9fe2a2c8c51b6b0bb8fe2ebdfadd23e165b773c
zztaouil <ztaouil@outlook.com>
2026-01-13 14:23:45 +0100

    [feat] add geometric transformations notebook

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from math import ceil
 
+
 def display_img(img, title=None, color_space_flag=True):
     # converting color space BGR (default for cv2.imread) to RGB (what plt expects)
     # example color spaces: Grayscale, HSV (Hue, Saturation, Value),
@@ -15,8 +16,9 @@ def display_img(img, title=None, color_space_flag=True):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(img)
     # plt.imshow(img[:,:,::-1])
-    plt.axis('off')
+    plt.axis("off")
     plt.show()
+
 
 def display_mul_img(imgs, titles=None, color_space_flag=True, rows_nb=1):
     if titles and len(imgs) != len(titles):
@@ -29,17 +31,23 @@ def display_mul_img(imgs, titles=None, color_space_flag=True, rows_nb=1):
         if color_space_flag:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         plt.imshow(img)
-        plt.axis('off')
+        plt.axis("off")
 
     plt.tight_layout(pad=1.0)
     plt.show()
     return 0
 
+
 def draw_noise(img):
     pixels_nb = img.size // 5
-    
+
     x = np.random.randint(0, img.shape[0], pixels_nb)
     y = np.random.randint(0, img.shape[1], pixels_nb)
-    
+
     img[x, y] = [0, 0, 0]
-    
+
+
+def draw_circle(img, coords, color=(0, 255, 0)):
+    # Draw a point at each coordinate in the list
+    for coord in coords:
+        cv2.circle(img, (int(coord[0]), int(coord[1])), radius=20, color=color, thickness=-1)
